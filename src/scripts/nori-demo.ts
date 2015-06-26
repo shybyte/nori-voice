a6e8927434b21ba25626cc8e386543786f8bd68d
shybyte <ein@volloeko.de>
2015-06-26 17:10:10 +0200

    Added ScriptProcessor

--- src/scripts/nori-demo.ts
+++ src/scripts/nori-demo.ts
@@ -1,28 +1,50 @@
 /// <reference path="utils" />
 
+
 module nori.demo {
   'use strict';
 
-  var attack=0.05;
-  var release=0.05;
-  var portamento=0.05;
-  var activeNotes : any[] = [];	// the stack of actively-pressed keys
+  var attack = 0.05;
+  var release = 0.05;
+  var portamento = 0.05;
+  var activeNotes:any[] = [];	// the stack of actively-pressed keys
 
-  var context = new AudioContext();
-  var oscillator = context.createOscillator();
+  var audioContext = new AudioContext();
+  var oscillator = audioContext.createOscillator();
   oscillator.type = 'sawtooth';
   oscillator.frequency.setValueAtTime(110, 0);
-  var envelope = context.createGain();
-  oscillator.connect(envelope);
-  envelope.connect(context.destination);
-  envelope.gain.value = 0.2;
-  oscillator.start(0);
+  //var envelope = audioContext.createGain();
+  //oscillator.connect(envelope);
+  //envelope.connect(audioContext.destination);
+  //envelope.gain.value = 0.2;
+  //oscillator.start(0);
+
+
+  var gain = audioContext.createScriptProcessor(1024, 1, 1);
+  gain.onaudioprocess = (function () {
+    var amplitude = 0.25;
+    var decay = Math.exp(-1.0 / (0.5 * audioContext.sampleRate));
+    console.log(decay);
+    return function (event:AudioProcessingEvent) {
+      var inp = event.inputBuffer.getChannelData(0);
+      var out = event.outputBuffer.getChannelData(0);
+      for (var i = 0, N = out.length; i < N; ++i, amplitude *= decay) {
+        out[i] = amplitude * inp[i];
+      }
+    };
+  }());
+
 
   console.log('Test');
 
   setTimeout(() => {
-    envelope.gain.value = 0;  // Mute the sound
+    oscillator.connect(gain);
+    gain.connect(audioContext.destination);
+    oscillator.start(audioContext.currentTime);
+    oscillator.stop(audioContext.currentTime + 1.0);
   }, 1000);
+
+
 }
 
 

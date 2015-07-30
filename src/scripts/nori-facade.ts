7bbad9029d1501d6efff60ad1b58de6a95a001f4
shybyte <ein@volloeko.de>
2015-07-30 11:01:12 +0200

    Create nori facade with nori.facade.startNoriVoice

--- src/scripts/nori-facade.ts
+++ src/scripts/nori-facade.ts
@@ -5,60 +5,60 @@
 module nori.facade {
   'use strict';
 
-  var nora = new syWRonan();
+  export function startNoriVoice() {
+    var nora = new syWRonan();
 
-  nora.texts[0] = "";
-  ronanCBInit(nora);
-  ronanCBSetSR(nora, 44100);
-  ronanCBSetCtl(nora, 4, 70);
-  ronanCBSetCtl(nora, 5, 100);
+    nora.texts[0] = "";
+    ronanCBInit(nora);
+    ronanCBSetSR(nora, 44100);
+    ronanCBSetCtl(nora, 4, 70);
+    ronanCBSetCtl(nora, 5, 100);
 
-  var audioContext = new AudioContext();
-  var oscillator = audioContext.createOscillator();
-  oscillator.type = 'sawtooth';
-  oscillator.frequency.setValueAtTime(175, 0);
+    var audioContext = new AudioContext();
+    var oscillator = audioContext.createOscillator();
+    oscillator.type = 'sawtooth';
+    oscillator.frequency.setValueAtTime(175, 0);
 
-  var gain = audioContext.createScriptProcessor(1024, 1, 1);
+    var noriVoiceAudioProcessor = audioContext.createScriptProcessor(1024, 1, 1);
 
-  gain.onaudioprocess = (function () {
-    var amplitude = 0.25;
-    var decay = Math.exp(-1.0 / (0.5 * audioContext.sampleRate));
-    //console.log(decay);
-    var counter = 0;
+    noriVoiceAudioProcessor.onaudioprocess = (function () {
+      var amplitude = 0.25;
+      var decay = Math.exp(-1.0 / (0.5 * audioContext.sampleRate));
+      //console.log(decay);
+      var counter = 0;
 
-    return function (event:AudioProcessingEvent) {
-      if (counter < 1000) {
-        ;
-        if (counter === 1) {
-          ronanCBNoteOn(nora);
+      return function (event:AudioProcessingEvent) {
+        if (counter < 1000) {
+          if (counter === 1) {
+            ronanCBNoteOn(nora);
+          }
+          counter++;
         }
-        counter++;
-      }
-      var inp = event.inputBuffer.getChannelData(0);
-      var out = event.outputBuffer.getChannelData(0);
-      ronanCBProcess(nora, inp, out);
-      for (var i = 0, N = out.length; i < N; ++i, amplitude *= decay) {
-        if (i % 128 === 0) {
-          ronanCBTick(nora);
+        var inp = event.inputBuffer.getChannelData(0);
+        var out = event.outputBuffer.getChannelData(0);
+        ronanCBProcess(nora, inp, out);
+        for (var i = 0, N = out.length; i < N; ++i, amplitude *= decay) {
+          if (i % 128 === 0) {
+            ronanCBTick(nora);
+          }
+          //out[i] = amplitude * inp[i];
+          out[i] = out[i] / 2;
         }
-        //out[i] = amplitude * inp[i];
-        out[i] = out[i] / 2;
+      };
+    }());
+
+
+    oscillator.start(audioContext.currentTime);
+    oscillator.connect(noriVoiceAudioProcessor);
+    noriVoiceAudioProcessor.connect(audioContext.destination);
+
+    return {
+      sing: (phonemes:string, freq:number) => {
+        nora.texts[0] = phonemes;
+        oscillator.frequency.setValueAtTime(freq, 0);
+        ronanCBSetCtl(nora, 4, 0);
       }
-    };
-  }());
-
-
-  oscillator.start(audioContext.currentTime);
-  oscillator.connect(gain);
-  gain.connect(audioContext.destination);
-
-  export function sing(phonemes:string, freq: number) {
-    nora.texts[0] = phonemes;
-    oscillator.frequency.setValueAtTime(freq, 0);
-    ronanCBSetCtl(nora, 4, 0);
+    }
   }
 
 }
-
-
-
